2e237d9f9705bf6397ff4b5c61cb83b58c6e20fb
kaitlinculligan <kaitlinculligan@gmail.com>
2024-03-02 15:58:20 -0700

    added in bootstrap

---
+++ my-app/src/Homepage.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function Homepage() {
+	return(
+		<div className="Homepage">
+			<p>
+			  Edit  and save to reload.
+			</p>
+
+
+		</div>
+	  );
+}
+
+export default Homepage;
