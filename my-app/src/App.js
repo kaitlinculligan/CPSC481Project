7d4b1fdeaf38399545764015a0df883d4821a4f5
kaitlinculligan <kaitlinculligan@gmail.com>
2024-03-03 14:32:37 -0700

    added search page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import {Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage.js'; // Fix the casing of the import statement
 import LoginPage from './pages/LoginPage.js'; // Fix the casing of the import statement
 import Favourites from './pages/Favourites.js'; // Fix the casing of the import statement
-
+import SearchPage from './pages/SearchPage.js'; // Fix the casing of the import statement
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage/>} />
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/favourites" element={<Favourites/>} />
+      <Route path="/search" element={<SearchPage/>} />
     </Routes>
   );
 }
