7d4a59366d6ca1a0c0b6a55e693688435e39ebe6
BrettLyle11 <122420347+BrettLyle11@users.noreply.github.com>
2024-03-03 13:47:08 -0700

    Updated login

--- my-app/src/pages/loginPage.js
+++ my-app/src/pages/loginPage.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import profilePic from "./Photos/homePage.png";
  
 function LoginPage() {
-  return <div>Login Page</div>;
+return <div className='col justify-conent-center' style={{ height: '100vh', width: '100vw' }}>
+    <div className='row justify-content-center' style={{margin:"25px",width:"100px",height:"100px", backgroundImage: `url(${profilePic})`, backgroundSize: 'cover',backgroundPosition: 'center',backgroundRepeat: 'no-repeat'}}>
+    </div>
+    <div className='row justify-content-center'>
+        <div className='col-4' >
+            <div className='row justify-content-center'style={{marginBottom:"10px"}}>
+                <h1>Login</h1>
+            </div>
+            <div className='row justify-content-center' >
+                <form>
+                    <div className='form-group'style={{marginBottom:"10px"}}>
+                        <label for='exampleInputEmail1'>Email address</label>
+                        <input type='email' className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' />
+                    </div>
+                    <div className='form-group' style={{marginBottom:"25px"}}>
+                        <label for='exampleInputPassword1'>Password</label>
+                        <input type='password' className='form-control' id='exampleInputPassword1' />
+                    </div>
+                    
+                    <button type='submit' className='btn btn-primary'style={{marginLeft:"50px",marginRight:"50px"}}>Submit</button>
+                    <button type='submit' className='btn btn-success'>Create Account</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>;
 }
 export default LoginPage;
