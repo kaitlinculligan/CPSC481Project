836f5ba305b30ade644dbe9033e06bb4a20e610a
BrettLyle11 <122420347+BrettLyle11@users.noreply.github.com>
2024-03-03 02:35:56 -0700

    Main page made!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,46 @@ import Test from './Test';
 import Homepage from './Homepage';
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import hypeImage from "./Photos/hype.png";
 
 function App() {
-  return <div><Button>Test Button</Button></div>
+  return <div style={{height:"700px",backgroundColor:"#10a690"}}>      
+  <div className="row" style={{height:"250px"}}>
+    <div className="col p-5" style={{maxWidth:"250px",height:"400px"}} >
+      <div className='row' style={{backgroundColor:"#3d94db",margin:"40px",padding: "20px",width:"100px",height:"100px",border: '10px solid black'}}>profile picture</div>
+      <div className='row' style={{backgroundColor:"#3d94db",margin:"40px",padding: "20px",width:"100px",height:"100px",border: '10px solid black'}}>favourites</div>
+    </div>
+    <div className='col justify-content-center' ></div>
+    <div className='col justify-content-right m-5' style={{height:"175px",maxWidth:"250px",backgroundColor:"#ba6856",border: '10px solid black'}} >
+    <div className="d-flex flex-column align-items-stretch">
+            <button type="button" className="btn btn-primary vertical-btn-group" style={{height:"52px"}}>Top</button>
+            <button type="button" className="btn btn-secondary vertical-btn-group " style={{height:"52px"}}>Middle</button>
+            <button type="button" className="btn btn-success vertical-btn-group" style={{height:"52px"}}>Bottom</button>
+          </div>
+    </div>
+  </div>
+  <div className='row justify-content-center p-2' >
+  <img src={hypeImage} className="img-fluid img-thumbnail rounded-circle" style={{width:"250px",height:"250px",borderColor:"black" }}></img>
+  </div>
+  <div className='row justify-content-center pb-5' >
+    <Button style={{width:"200px"}}>Test Button</Button>
+  </div>
+  <div className='row justify-content-center' >
+    <div className='col-4' style={{maxWidth:"375px",height:"250px",backgroundColor:"#ba6856",border: '10px solid black'}} >
+      house 1
+    </div>
+    <div className='col-4' style={{maxWidth:"375px",height:"250px",backgroundColor:"#ba6856",border: '10px solid black'}} >
+      house 2
+    </div>
+    <div className='col-4' style={{maxWidth:"375px",height:"250px",backgroundColor:"#ba6856",border: '10px solid black'}} >
+      house 3
+    </div>
+    <div className='col-4' style={{maxWidth:"375px",height:"250px",backgroundColor:"#ba6856",border: '10px solid black'}} >
+      house 4
+    </div>
+  </div>
+
+</div>
 }
 
 export default App;
