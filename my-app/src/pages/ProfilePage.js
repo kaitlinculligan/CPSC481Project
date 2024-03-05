6205128805d6a0a136c4d96c4b12c8a2d46a7af5
BrettLyle11 <122420347+BrettLyle11@users.noreply.github.com>
2024-03-04 21:51:18 -0700

    updated

--- my-app/src/pages/ProfilePage.js
+++ my-app/src/pages/ProfilePage.js
@@ -2,7 +2,7 @@ import profilePic from "./Photos/profileLogo.png";
 import homePic from "./Photos/homePage.png";
 import hypeImage from "./Photos/hype.png";
 import favourites from "./Photos/favourites.png";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 function ProfilePage() {
   const navigateToPage = (url) => {
     // This will reload the page and navigate to the new URL.
@@ -10,10 +10,10 @@ function ProfilePage() {
   };
 
   return (
-    <div className="flex-row d-flex vw-100 vh-100" style={{ background: "linear-gradient(#10a690, white)" }}>
+    <div className="flex-row d-flex vw-100 vh-100" style={{ background: "linear-gradient(rgba(16, 166, 144, 0.5), white)" }}>
       <div className="w-25 d-flex flex-column justify-content-between">
-        <div style={{ height: "85%" }}>
-          <div className="w-100 p-3 h-auto flex-row d-flex justify-content-around">
+        <div className=" h-100">
+          <div className="w-100 p-3 h-auto flex-row d-flex justify-content-evenly pt-5">
             <div
               className="col-4"
               onClick={() => navigateToPage("./")}
@@ -51,10 +51,12 @@ function ProfilePage() {
         </div>
       </div>
       <div className="w-75 flex-column d-flex align-items-center pt-5">
+        <Card className="p-5 rounded-3">
         <h1>Current House Bookings</h1>
-        <div className="w-50 h-auto border-5 border-black border rounded-2 p-2" style={{ minHeight: "100px" }}>
+        <div className="h-auto border-5 border-black border rounded-2 p-2 mt-5" style={{ minHeight: "100px" }}>
           There are no current house bookings.
         </div>
+        </Card>
       </div>
     </div>
   );
