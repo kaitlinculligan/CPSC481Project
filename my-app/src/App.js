424b87f589dc515a432bb10957ffff2c186f0482
Kaitlin Culligan <81047241+kaitlinculligan@users.noreply.github.com>
2024-03-04 22:18:36 -0700

    Update App.js to use loginPage.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage.js'; // Fix the casing of the import statement
 
-import LoginPage from './pages/LoginPage.js'; // Fix the casing of the import statement
+import LoginPage from './pages/loginPage.js'; // Fix the casing of the import statement
 import Favourites from './pages/Favourites.js';
 import SearchPage from './pages/SearchPage.js';
 import SearchResults from './pages/SearchResults.js'; 
