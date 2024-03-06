b1a1deaa29d5f1f570dd8d6b10bebe0ed7a0ec08
BrettLyle11 <122420347+BrettLyle11@users.noreply.github.com>
2024-03-05 19:38:37 -0700

    better house card font

--- my-app/src/pages/HouseCard.js
+++ my-app/src/pages/HouseCard.js
@@ -5,7 +5,7 @@ function HouseCard({ Name, Photo, Price, Description }) {
     <div className="w-100 h-100  d-flex align-content-center border-3 border " style={{ maxWidth:"400px", marginLeft: "500px", marginRight: "500px", marginTop: "100px" }}>
       <Card className="w-100 w-100 d-flex flex-column  align-items-center h-100" style={{ borderColor: "grey", cursor: "pointer", height: "100%", height: "100%" }}>
         <div className="row bg-black w-100 border-5 border-success border" style={{ minHeight: "15%" }}>
-          <p1 style={{ fontWeight: "bold", color: "white" }}>{Name}</p1>
+          <h1 style={{ fontWeight: "bold", color: "white" }}>{Name}</h1>
         </div>
         <div className="bg-success row w-100" style={{ minHeight: "35%" }}>
           <img
@@ -15,9 +15,9 @@ function HouseCard({ Name, Photo, Price, Description }) {
           />
         </div>
         <div className=" bg-black row w-100 border-5 border-success border" style={{ minHeight: "15%" }}>
-          <p1 style={{ fontWeight: "bold", color: "white" }}>{Price}</p1>
+          <h1 style={{ fontWeight: "bold", color: "white" }}>{Price}</h1>
         </div>
-        <div className="bg-black row w-100" style={{ minHeight: "35%" }}>
+        <div className="bg-black row w-100 border-5 border-success border" style={{ minHeight: "35%" }}>
           <p2 style={{ fontWeight: "bold", color: "white" }}>{Description}</p2>
         </div>
       </Card>
