dba6eb5d75819d1f134a9fc78dc82665c73929b5
ananditamahika <84046117+ananditamahika@users.noreply.github.com>
2024-03-05 20:56:57 -0700

    added help icon and made gradient, changed spacings

--- my-app/src/pages/MainPage.js
+++ my-app/src/pages/MainPage.js
@@ -9,6 +9,7 @@ import house3 from "./Photos/house3.png";
 import house4 from "./Photos/house4.png";
 import profilePic from "./Photos/profileLogo.png";
 import favourites from "./Photos/favourites.png";
+import help from "./Photos/help_icon.png";
 
 function MainPage() {
   const navigateToPage = (url) => {
@@ -17,9 +18,9 @@ function MainPage() {
   };
 
   return (
-    <div style={{ backgroundColor: "#10a690" }}>
-      <div className="row" style={{ height: "250px" }}>
-        <div className="col p-5" style={{ maxWidth: "250px", height: "400px" }}>
+    <div style={{ background: "linear-gradient(rgba(16, 166, 144, 0.5), white)" }}>
+      <div className="row" style={{ height: "400px" }}>
+        <div className="col p-5" style={{ maxWidth: "250px", height: "600px" }}>
           <strong>
             <p1>Login</p1>
           </strong>
@@ -48,10 +49,30 @@ function MainPage() {
             style={{
               cursor: "pointer",
               marginLeft: "1px",
+              marginBottom: "40px",
               padding: "20px",
               width: "100px",
               height: "100px",
               backgroundImage: `url(${favourites})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <strong>
+            <p1>Need Help?</p1>
+          </strong>
+          <div
+            className="row"
+            onClick={() => navigateToPage("./favourites")}
+            style={{
+              cursor: "pointer",
+              marginLeft: "1px",
+              marginBottom: "40px",
+              padding: "20px",
+              width: "100px",
+              height: "100px",
+              backgroundImage: `url(${help})`,
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
@@ -81,10 +102,12 @@ function MainPage() {
             </button>
           </div>
         </div>
+
+        <div className="col justify-content-center align-items-center">
+          <img src={hypeImage} className="img-fluid " style={{ width: "450px", height: "450px", borderColor: "black" }}></img>
+        </div>
       </div>
-      <div className="row justify-content-center" style={{ paddingBottom: "50px", }}>
-        <img src={hypeImage} className="img-fluid " style={{ width: "400px", height: "400px", borderColor: "black" }}></img>
-      </div>
+
       <div className="row justify-content-center pb-5">
         <Button style={{ width: "200px", backgroundColor:"black" }}>Discover Your Future House!</Button>
       </div>
