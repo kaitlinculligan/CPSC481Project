b6cf747b0d89a4f02a2858f2effd5942f9b66cdc
Hera-Naeem <hera.naeem@ucalgary.ca>
2024-04-03 21:53:45 -0600

    Update SearchPage.js

--- my-app/src/pages/SearchPage.js
+++ my-app/src/pages/SearchPage.js
@@ -49,6 +49,12 @@ function SearchPage() {
 				<h3># of Bed</h3>
 				<select className='searchForm'>
 					<option>Any</option>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>5+</option>
 				</select>
 				</div>
 				<div className='col'></div>
@@ -56,6 +62,12 @@ function SearchPage() {
 				<h3># of Bath</h3>
 				<select className='searchForm'>
 					<option>Any</option>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>5+</option>
 				</select>
 				</div>
 			</div>
